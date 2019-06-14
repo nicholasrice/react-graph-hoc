9003b2d2d13699534a3c5f4ca126a4689a28a9e8
Nick Rice <nicholas.rice119@gmail.com>
2019-06-13 17:05:43 -0700

    adding initial graph

---
+++ src/graph-hoc.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const context = React.createContext({depth: 0});
+
+const { Consumer, Provider } = context;
+
+class Graph extends React.Component {
+    render() {
+        return (
+            <Consumer>
+                {(context) => {
+                    return (
+                        <Provider value={{depth: context.depth + 1}}>
+                            <div style={{padding: "0 8px"}}>
+                                Depth
+                                {context.depth + 1}
+                                {this.props.children}
+                            </div>
+                        </Provider>
+                    )
+                }}
+            </Consumer>
+        )
+    }
+}
+
+Graph.contextType = context;
+
+export default Graph;
